8b68310d1daa3b037600f703a892f39deb26b360
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-06 09:15:11 +0200

    changed it

--- src/features/NavigationBar/index.js
+++ src/features/NavigationBar/index.js
@@ -18,7 +18,7 @@ import { ActorPage } from "../People/ActorPage/ActorPage";
 
 export const NavigationBar = () => {
   return (
-    <HashRouter basename="Movie-Browser">
+    <HashRouter basename="/Movie-Browser">
       <Header>
         <NavBar>
           <NavList>
