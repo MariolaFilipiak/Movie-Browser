e5fed2432bd4a94afdcd62e5fff4404b4b69c17e
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-06 08:14:52 +0200

    I removed saga , store and slice and QueryClientProvider added

--- src/core/rootSaga.js
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-import { moviesSaga } from "../feature/Movie/moviesSaga";
-import { peopleSaga } from "../feature/People/peopleSaga";
-import { moviePageSaga } from "../feature/Movie/MoviePage/moviePageSaga";
-import { actorSaga } from "../feature/People/ActorPage/actorSaga";
-
-export default function* rootSaga() {
-  yield all([moviesSaga(), peopleSaga(), actorSaga(), moviePageSaga()]);
-}
